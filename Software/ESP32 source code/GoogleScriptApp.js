1ab8f2e8f924d70c7fa57d1d83d29fca39528700
enriquefloresmedina <116412077+enriquefloresmedina@users.noreply.github.com>
2022-12-19 19:29:44 -0800

    Add files via upload

---
+++ Software/ESP32 source code/GoogleScriptApp.js
@@ -0,0 +1,25 @@
+function doPost(e) {
+  var myFoldername = e.parameter.myFoldername;
+  var myFile = e.parameter.myFile;
+  var myFilename = e.parameter.myFilename;
+  
+  var contentType = myFile.substring(myFile.indexOf(":")+1, myFile.indexOf(";"));
+  var data = myFile.substring(myFile.indexOf(",")+1);
+  data = Utilities.base64Decode(data);
+  var blob = Utilities.newBlob(data, contentType, myFilename);
+  
+  // Save a captured image to Google Drive.
+  var folder, folders = DriveApp.getFoldersByName(myFoldername);
+  if (folders.hasNext()) {
+    folder = folders.next();
+  } else {
+    folder = DriveApp.createFolder(myFoldername);
+  }
+  var file = folder.createFile(blob);    
+  file.setDescription("Uploaded by " + myFilename);
+  
+  var imageID = file.getUrl().substring(file.getUrl().indexOf("/d/")+3,file.getUrl().indexOf("view")-1);
+  var imageUrl = "https://drive.google.com/uc?authuser=0&id="+imageID;
+      
+  return  ContentService.createTextOutput(myFoldername+"/"+myFilename+"\n"+imageUrl+"\n");
+}
