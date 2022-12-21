13343201f59156b40f01de179cfe083ce636db04
enriquefloresmedina <116412077+enriquefloresmedina@users.noreply.github.com>
2022-12-20 17:03:35 -0800

    Add files via upload

---
+++ Software/Python code/Download images from Google Drive/main.py
@@ -0,0 +1,24 @@
+# Google Drive API authentication
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+# Delay
+import time
+
+# Initialize Google Drive API
+gauth = GoogleAuth()
+drive = GoogleDrive(gauth)
+
+# Folder ID to download images from
+folder = '1tF5At-fiS6HUrIEEOK33T1aCAytiAxSh'
+
+while True:
+	# Create a list of the files in the folder
+	file_list = drive.ListFile({'q' : f"'{folder}' in parents and trashed=false"}).GetList()
+	for index, file in enumerate(file_list):
+		print(index+1, 'Image downloaded : ', file['title'])
+		# Download every file in the folder
+		file.GetContentFile(file['title'])
+		# Delete file from Google Drive to save space
+		file.Delete()
+		# Wait for 2 seconds for energy saving
+		time.sleep(2)
